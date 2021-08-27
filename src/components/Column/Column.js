3261dab1c3d5331a34e1d30ed21809fd7ae6d600
Daria Górecka <dmgorecka@gmail.com>
2021-08-27 11:39:16 +0200

    Task 12.3

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,13 +3,10 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import { settings } from '../../data/dataStore.js';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-    state = {
-      cards: this.props.cards || [],
-    }
     static propTypes = {
       title: PropTypes.node,
       cards: PropTypes.array,
@@ -19,36 +16,24 @@ class Column extends React.Component {
     static defaultProps = {
       icon: settings.defaultColumnIcon,
     }
-    addCard(title) {
-      this.setState(state => (
-        {
-          cards: [
-            ...state.cards,
-            {
-              key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-              title,
-            },
-          ],
-        }
-      ));
-    }
     render(){
+      const {title, icon, cards} = this.props;
       return(
         <section className={styles.component}>
           <h3 className={styles.title}>
-            {this.props.title}
+            {title}
             <span className={styles.icon}>
-              <Icon name={this.props.icon}/>
+              <Icon name={icon}/>
             </span>
           </h3>
           <div className={styles.cards}>
-            {this.state.cards.map(cardData => (
+            {cards.map(cardData => (
               <Card key={cardData.id} {...cardData} />
             ))}
           </div>
-          <div className={styles.creator}>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-          </div>
+          {/* <div className={styles.creator}>
+             <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div> */}
         </section>
       );
     }
