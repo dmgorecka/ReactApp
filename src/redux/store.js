871e20f95a167f212e5ad5572dedc52f82f7d5d8
Daria Górecka <dmgorecka@gmail.com>
2021-08-31 14:53:42 +0200

    Solving import bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {combineReducers, createStore} from 'redux';
-import initialStoreData from '../src/data/dataStore';
+import initialStoreData from '../data/dataStore';
 import columnsReducer from './columnsRedux.js';
 import cardsReducer from './cardsRedux.js';
 import searchStringReducer from './searchStringRedux.js';
