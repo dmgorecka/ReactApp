3e3c034149b3b211c9cb10177d48015090fd5e97
Daria Górecka <dmgorecka@gmail.com>
2021-08-31 09:58:57 +0200

    Add cards redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ import styles from './List.scss';
 import PropTypes from 'prop-types';
 import Column from '../Column/ColumnContainer.js';
 import {settings} from '../../data/dataStore.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
@@ -15,29 +15,30 @@ class List extends React.Component {
       image: PropTypes.string,
       description: PropTypes.node,
       columns: PropTypes.array,
+      addColumn: PropTypes.func,
     }
-      static defaultProps = {
-        description: settings.defaultListDescription,
-      }
-      render() {
-        const {title, image, description, columns} = this.props;
-        return (
-          <section className={styles.component}>
-            <Hero titleText={title} image={image} />
-            <div className={styles.description}>
-              {ReactHtmlParser(description)}
-            </div>
-            <div className={styles.columns}>
-              {columns.map(columnData => (
-                <Column key={columnData.id} {...columnData} />
-              ))}
-            </div>
-            {/* <div className={styles.creator}>
-              <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-              </div> */}
-          </section>
-        );
-      }
+    static defaultProps = {
+      description: settings.defaultListDescription,
+    }
+    render() {
+      const {title, image, description, columns, addColumn} = this.props;
+      return (
+        <section className={styles.component}>
+          <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+          { <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div> }
+        </section>
+      );
+    }
 }
 
 export default List;
